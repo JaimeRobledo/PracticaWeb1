c7072c0b8a732e8ec8abef65f4a339dfd56f63ea
Jaime Robledo <114981240+JaimeRobledo@users.noreply.github.com>
2025-03-31 16:47:25 +0200

    actualizar datos compañia en el controller

--- controlers/users.js
+++ controlers/users.js
@@ -141,7 +141,27 @@ const updateDatosPersonales = async (req, res) => {
 
 const updateDatosCompany = async (req, res) => {
 
+    const {autonomo, name, cif, adress} = req.body
+    if (!req.headers.authorization) {
+        handleHttpError(res, "NOT_TOKEN", 401)
+        return
+    }
+    // Nos llega la palabra reservada Bearer (es un estándar) y el Token, así que me quedo con la última parte
+    const token = req.headers.authorization.split(' ').pop()
+    //Del token, miramos en Payload (revisar verifyToken de utils/handleJwt)
+    const dataToken = await verifyToken(token)
+    console.log(dataToken)
+    if(!dataToken) {
+        handleHttpError(res, "INVALID_TOKEN", 401)
+        return
+    }
+
+    const user = await userModel.updateOne({_id: dataToken._id}, {nombre, apellidos, nif})
+    if (!user) {
+        return res.status(404).json({ message: "Usuario no encontrado" });
     
+    }
+    res.status(201).json({message: "Usuario actualizado correctamente:",user, nombre, apellidos, nif})
 
 }
 
