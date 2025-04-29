5153f43594c3b1aa71a1b0f69e251163b57a636c
Jaime Robledo <114981240+JaimeRobledo@users.noreply.github.com>
2025-04-29 18:03:19 +0200

    controller clientes creados

--- controlers/clients.js
+++ controlers/clients.js
@@ -3,9 +3,29 @@ const {encrypt, compare} = require('../utils/validatePassword.js')
 const {tokenSign, verifyToken} = require('../utils/encargarseJwt.js')
 
 const crearCliente = async (req, res) => {
-    const {nombre, email, telefono, direccion, usuarioId, companiaId} = req.body
-    
+    try {
+        const {nombre, email, telefono, direccion, companiaId} = req.body
+        const usuarioId = req.user.id
+
+        const clienteExistente = await clientModel.findOne({email, usuarioId, companiaId})
+        if (clienteExistente) {
+            return res.status(400).json({message: "El cliente ya existe"})
+        }
+        const nuevoCliente = new clientModel({
+            nombre,
+            email,
+            telefono,
+            direccion,
+            usuarioId,
+            companiaId
+        })    
+        await nuevoCliente.save()
+        return res.status(201).json({message: "Cliente creado", cliente: nuevoCliente})
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({message: "Error al crear el cliente"})
+    }
     
 }
 
-module.exports = {}
+module.exports = {crearCliente}
--- router/clients.js
+++ router/clients.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const {crearCliente} = require('../controlers/clients.js');
+const {validatorCrearCliente} = require('../validators/validatorClients.js');
 
 const clientRouter = express.Router();
 
