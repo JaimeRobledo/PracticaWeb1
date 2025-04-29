71a3372a24feb51155092470fefc3a158a05d789
Jaime Robledo <114981240+JaimeRobledo@users.noreply.github.com>
2025-04-29 09:38:02 +0200

    configuracion app.js

--- app.js
+++ app.js
@@ -8,8 +8,19 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api/users', userRouter)
+const swaggerUi = require("swagger-ui-express")
+const swaggerSpecs = require("./docs/swagger")
+app.use(express.static("storage")) // http://localhost:3000/file.jpg
 const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log("Servidor escuchando en el puerto " + port)
 })
 dbConnect()
+
+app.use("/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerSpecs)
+    )
+    app.use("/api", require("./router"))
+
+module.exports = {app, server};
