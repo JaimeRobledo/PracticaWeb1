1134ce6c3eec4d110325b4b4c5f604b9faf32d2b
Jaime Robledo <114981240+JaimeRobledo@users.noreply.github.com>
2025-04-29 23:37:23 +0200

    multer duplicado arreglado

--- router/users.js
+++ router/users.js
@@ -4,20 +4,7 @@ const { validatorRegister, validatorCodigo, validatorLogin, validatorDatosPerson
 const userRouter = express.Router();
 
 const { uploadMiddleware} = require("../utils/handleStorage.js")
-const multer = require("multer")
-const storage = multer.diskStorage({
-    destination:function(req, file, callback){ //Pasan argumentos automáticamente
-        const pathStorage = __dirname+"/../storage"
-        callback(null, pathStorage) //error y destination
-    },
-    filename:function(req, file, callback){ //Sobreescribimos o renombramos
-        //Tienen extensión jpg, pdf, mp4
-        const ext = file.originalname.split(".").pop() //el último valor
-        const filename = "file-"+Date.now()+"."+ext
-        callback(null, filename)
-    }
-}) //Middleware entre la ruta y el controlador
-//hasta aquí
+
 
 userRouter.use(express.json());
 
