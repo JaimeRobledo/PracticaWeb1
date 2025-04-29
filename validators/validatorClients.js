a95d136e574e8f1cd00a9b51e6524c1c7e0b9c19
Jaime Robledo <114981240+JaimeRobledo@users.noreply.github.com>
2025-04-29 18:24:26 +0200

    validator mejorado

--- validators/validatorClients.js
+++ validators/validatorClients.js
@@ -2,7 +2,16 @@ const { check } = require("express-validator")
 const {validateResults} = require("../utils/encargarseValidacion.js")
 
 const validatorCrearCliente = [
-    
+
+    check("nombre").exists().notEmpty(),
+    check("cif").exists().notEmpty(),
+    check("address").exists().notEmpty(),
+    check("address.street").exists().notEmpty(),
+    check("address.number").exists().notEmpty(),
+    check("address.postal").exists().notEmpty(),
+    check("address.city").exists().notEmpty(),
+    check("address.province").exists().notEmpty(),
+    check("companiaId").optional().notEmpty(),
 
     (req, res, next) => {
         return validateResults(req, res, next)
@@ -10,4 +19,4 @@ const validatorCrearCliente = [
 
 ]
 
-module.exports = {}
+module.exports = {validatorCrearCliente}
