0d23893c976665274ac3e9a63e3d5dbfbef2893d
Jaime Robledo <114981240+JaimeRobledo@users.noreply.github.com>
2025-05-01 12:45:03 +0200

    intento de usar matched data, no funciona

--- controlers/clients.js
+++ controlers/clients.js
@@ -1,4 +1,5 @@
 const clientModel = require('../models/clients.js')
+const { matchedData } = require("express-validator")
 const {encrypt, compare} = require('../utils/validatePassword.js')
 const {tokenSign, verifyToken} = require('../utils/encargarseJwt.js')
 const { handleHttpError } = require('../utils/handleError.js');
